2373878c638cdeac549b756ad5955db80d9c1b4c
DavidCorrea <davidlevycostabr.7@gmail.com>
2025-12-11 16:58:42 -0300

    works on web now but with some artifacts

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,11 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-/**
- * @type {import('expo/metro-config').MetroConfig}
- */
-const { getDefaultConfig } = require('expo/metro-config')
-const { withTamagui } = require('@tamagui/metro-plugin')
+const { getDefaultConfig } = require('expo/metro-config');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
-  // [Web-only]: Enables CSS support in Metro.
   isCSSEnabled: true,
-})
+});
 
-config.resolver.sourceExts.push('mjs')
+config.resolver.assetExts.push("wasm");
 
-module.exports = withTamagui(config, {
-  components: ['tamagui'],
-  config: './tamagui.config.ts',
-  outputCSS: './tamagui-web.css',
-  cssInterop: true,
-})
+module.exports = config;
